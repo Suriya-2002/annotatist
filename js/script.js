eb95a00b0b7aa8ed84b3c45e513f15440e03b909
Suriya KS <suriya2002.cse@gmail.com>
2021-06-07 12:38:09 +0530

    Designed control buttons and fixed user-box color

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
 document.querySelector('.navigation__lines').addEventListener('click', () => {
     document.querySelector('.navigation').classList.toggle('navigation--active');
+    document.querySelector('.control-buttons').classList.toggle('control-buttons--shrink');
 });
